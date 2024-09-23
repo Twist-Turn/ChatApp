cfc7ed9867a88af3f2393357853cd597776ea0dd
Twist-Turn <dineshdk2108@gmail.com>
2024-09-23 16:13:43 +0530

    another change

--- Backend/SocketIO/server.js
+++ Backend/SocketIO/server.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "https://chatapp-1-00tj.onrender.com",
+    origin: "http://localhost:3001",
     methods: ["GET", "POST"],
   },
 });
